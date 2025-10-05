56923f24dead87dc513e0445ce4db0206f05092e
vglena <bisont55@gmail.com>
2025-10-05 19:18:12 +0200

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,130 @@
+import copy
+import math
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    x_count = sum(row.count(X) for row in board)
+    o_count = sum(row.count(O) for row in board)
+    return X if x_count == o_count else O
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    return {(i, j)
+            for i in range(3)
+            for j in range(3)
+            if board[i][j] == EMPTY}
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    i, j = action
+    if board[i][j] != EMPTY:
+        raise ValueError("Invalid move")
+
+    new_board = copy.deepcopy(board)
+    new_board[i][j] = player(board)
+    return new_board
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    # Check rows and columns
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != EMPTY:
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] != EMPTY:
+            return board[0][i]
+
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] != EMPTY:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != EMPTY:
+        return board[0][2]
+
+    return None
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    return winner(board) is not None or all(cell != EMPTY for row in board for cell in row)
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    w = winner(board)
+    if w == X:
+        return 1
+    elif w == O:
+        return -1
+    else:
+        return 0
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+
+    if terminal(board):
+        return None
+
+    current_player = player(board)
+
+    def max_value(state):
+        if terminal(state):
+            return utility(state), None
+        v = -math.inf
+        best_action = None
+        for action in actions(state):
+            min_v, _ = min_value(result(state, action))
+            if min_v > v:
+                v = min_v
+                best_action = action
+        return v, best_action
+
+    def min_value(state):
+        if terminal(state):
+            return utility(state), None
+        v = math.inf
+        best_action = None
+        for action in actions(state):
+            max_v, _ = max_value(result(state, action))
+            if max_v < v:
+                v = max_v
+                best_action = action
+        return v, best_action
+
+    if current_player == X:
+        _, action = max_value(board)
+    else:
+        _, action = min_value(board)
+
+    return action
+
